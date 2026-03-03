54e8a008889b5317b7a165b5b6fc1ff4d74c028f
ansarais <sheikh.ansar016@gmail.com>
2026-03-03 17:13:14 +0530

    Loistic Regression is created

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,14 +1,22 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 # Load the dataset
 data = pd.read_csv('diabetes.csv')
 print(data.head())# Define the features and target variable
 X = data.drop('Outcome', axis=1)    
 y = data['Outcome']
-# Create and fit the linear regression model
-model = LinearRegression()
-model.fit(X, y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create and fit the logisticregression model
+model = LogisticRegression()
+model.fit(X_train, y_train)
 # Make predictions
-predictions = model.predict(X)
-print(predictions)
+predictions = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, predictions)
+print(f'Accuracy: {accuracy:.2f}')
+
